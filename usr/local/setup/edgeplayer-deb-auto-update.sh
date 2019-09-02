ab1dbae1c5a194aa0c9a67070da09c091f40d159
venkatesh <vsanjeevagowda@qwinix.io>
2019-09-02 12:13:07 +0530

    working cmmit

---
+++ usr/local/setup/edgeplayer-deb-auto-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+exec &>> /var/log/edgeplayer.log
+
+
+echo "[`date`][RUNNING] Updating repo index..."
+sudo apt-get update
+
+echo "[`date`][RUNNING] Installing deb-auto-update..."
+sudo apt-get install deb-auto-update
